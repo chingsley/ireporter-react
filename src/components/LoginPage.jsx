338187186fdb40bb1acfd044f9479a64405a9ae9
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro.local>
2019-04-14 00:17:07 +0100

    feat(user-login): implement user login feature
    - set up the components that make up the frontend
    - make the connection to the backend to login a user
    - style the sign up page using sass
    
    [Delivers #164040674]

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,39 +1,6 @@
-// import axios from 'axios';
 import React from 'react';
-import { connect } from 'react-redux';
-import RegisterForm from './AuthForm';
-import { signUpUser } from '../actions/auth';
+import AuthForm from './AuthForm';
 
-export class SignUpPage extends React.Component {
-  constructor(props) {
-    super(props);
+const LoginPage = props => <AuthForm formType="login" {...props} />;
 
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(userData) {
-    const { signUpUser: registerUser, history } = this.props;
-    registerUser(userData);
-    history.push('/signup');
-  }
-
-  render() {
-    return (
-      <div>
-        <RegisterForm
-          onSubmit={this.onSubmit}
-          formType="login" // possible values are "registration" or "login"
-        />
-      </div>
-    );
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-  signUpUser: userData => dispatch(signUpUser(userData))
-});
-
-export default connect(
-  undefined,
-  mapDispatchToProps
-)(SignUpPage);
+export default LoginPage;
